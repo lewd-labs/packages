5f53a93a4b34b6718a63904db91b295964172c8d
ThatGuyJamal <jamallyons8@gmail.com>
2021-08-19 16:29:17 -0400

    Adding logger

---
+++ README.md
@@ -0,0 +1,10 @@
+Packages is a public repo for the Taminaru Discord Bot. 
+
+This repo is for the bot and its dependencies. I dont want to clutter the core code so im making this.
+
+### packages 
+
+- Logger 
+- MongoDb provider
+
+more will be added
---
+++ index.ts
@@ -0,0 +1,14 @@
+/**
+ * Packages export file
+ * 
+ * ? Warning. These packages are editted to work with https://github.com/taminaru specifically meaning there ordinal functions from source have been changed. Only use this code in testing environments. 
+ * 
+ * Also the packages expect you to have the node_modules and there required code. We do not install them for you.
+ */
+
+/**
+ * @param {Class} Logger
+ * @description
+ */
+
+export { Logger } from "./logger/Logger"
