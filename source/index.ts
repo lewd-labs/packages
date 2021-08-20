ea3be8b3fdac352219a2ae235dd953dd5b8b4f4a
ThatGuyJamal <jamallyons8@gmail.com>
2021-08-20 07:53:20 -0400

    Update index.ts

--- source/index.ts
+++ source/index.ts
@@ -1,12 +1,14 @@
-export * from "./logger/index";
-export * from "./fapmongo/index";
+// Logger
+export { Logger } from "./logger/index"
+// Fapmongo db
+export { Database } from "./fapmongo/index"
 // MongoDB
 export { MongooseProvider } from "./provider"
 
 // giveaways
 export {
-    GiveawayClientOptions,
-    GiveawayClientSchema,
-    StartOptions,
-    GiveawayClient,
-} from "./giveaways/index";
+	GiveawayClientOptions,
+	GiveawayClientSchema,
+	StartOptions,
+	GiveawayClient,
+} from "./giveaways/index"
